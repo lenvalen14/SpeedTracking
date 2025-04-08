f36c6d40d5a47e8af7a283f70993ee62f420bf69
Nguyễn Lê Hiếu Nhi <nlhieunhi1402@gmail.com>
2025-04-08 17:12:19 +0700

    add validation for DTO

--- src/main/java/edu/ut/its/models/dtos/requests/StreetCreateRequest.java
+++ src/main/java/edu/ut/its/models/dtos/requests/StreetCreateRequest.java
@@ -1,5 +1,7 @@
 package edu.ut.its.models.dtos.requests;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,6 +16,11 @@ public class StreetCreateRequest {
     @NotBlank(message = "Street name is required")
     private String name;
     private String area;
+
+    @Min(value = 0, message = "Speed limit must be at least 0")
+    @Max(value = 300, message = "Speed limit must be not exceed 300")
     private int speedLimit;
+
+    @Min(value = 0, message = "Camera count must be at least 0")
     private int cameraCount;
 }
