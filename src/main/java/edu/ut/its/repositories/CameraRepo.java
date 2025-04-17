5bb3addca029d96133e5a194e33d77235bcdfe77
AKA-Truc <pthanhtruca3@gmail.com>
2025-04-17 19:41:51 +0700

    update getAllCamera

--- src/main/java/edu/ut/its/repositories/CameraRepo.java
+++ src/main/java/edu/ut/its/repositories/CameraRepo.java
@@ -1,7 +1,10 @@
 package edu.ut.its.repositories;
 
+import edu.ut.its.models.entities.Account;
 import edu.ut.its.models.entities.Camera;
 import edu.ut.its.models.entities.Street;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,7 +13,7 @@ import java.util.Optional;
 
 @Repository
 public interface CameraRepo extends MongoRepository<Camera, String> {
-    List<Camera> findAllByStatusTrue();
+    Page<Camera> findAllByStatusTrue(Pageable pageable);
     Optional<Camera> findByCameraIdAndStatusTrue(String id);
     long countByStreetAndStatusTrue(Street street);
     long countByStreet_StreetIdAndStatusTrue(String streetId);
